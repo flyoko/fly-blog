85ad6a6331b693a930e9719e026e8ef342dfb1c5
fly <99939664+flyoko@users.noreply.github.com>
2026-08-06 05:32:11 +0800

    移除公开页面的 GitHub 元数据

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
 				{ name: 'author', content: [blogConfig.author.name, blogConfig.author.email].filter(Boolean).join(', ') },
 				{ name: 'color-scheme', content: 'light dark' },
 				// 此处为元数据的生成器标识，不建议修改
-				{ 'name': 'generator', 'content': `${pascalCase(packageJson.name)} ${packageJson.version}`, 'data-github-repo': packageJson.homepage },
+				{ name: 'generator', content: `${pascalCase(packageJson.name)} ${packageJson.version}` },
 				{ name: 'mobile-web-app-capable', content: 'yes' },
 			],
 			link: [
